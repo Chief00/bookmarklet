33c41020d7c369844d31ebac09a073608b03b254
Joshua Molloy <josh.molloy@ddc-as.com>
2022-08-09 13:38:08 +0100

    Removed non-relevant folders

--- Tinder/TinderDeBlur.js
+++
@@ -1,7 +0,0 @@
-var imag = document.getElementsByClassName("Expand enterAnimationContainer")
-for (let i = 0; i < imag.length; i++) {
-	imag_div = imag[i].getElementsByTagName("div")[0]
-	console.log(imag_div.classList)
-	imag_div.className = "Bdrs(8px) Bgz(cv) Bgp(c) Ov(h) StretchedBox Ir(p) Cnt($blank)::a StretchedBox::a Bg($inherit)::a Scale(1.3)::a Scale(1.2)::a--s Blur(0px)::a"
-}
-
